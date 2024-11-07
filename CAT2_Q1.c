b5b079da920622ad554e3bef4973eddd23d8e5dd
theestarboy <muthonivincent15@gmail.com>
2024-11-08 01:33:53 +0300

    CAT2_Q1.c

--- CAT2_Q1.c
+++ CAT2_Q1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+# include <string.h>
 
+struct employee{
+char name[25];
+int id;
+char department[20];
+float salary;
+char email[50];
+
+}employee1;
+
+int main(){
+
+strcpy(employee1.name,"John Doe");
+employee1.id=12345;
+strcpy(employee1.department,"Human Resources");
+employee1.salary=55000.50;
+strcpy(employee1.email,"John.doe@company.com");
+
+printf("Name :%s\n",employee1.name);
+printf("Id :%d\n",employee1.id);
+printf("Department: %s\n",employee1.department);
+printf("Salary :%.2f\n", employee1.salary);
+printf("Email : %s\n", employee1.email);
+
+return 0;
+}
+    
